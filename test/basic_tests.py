aacfe8f032e50de166b64f1ee74382aa25aca89a
r3 <wojciechregula@yahoo.pl>
2017-03-26 00:33:22 +0100

    Translated tests

--- test/basic_tests.py
+++ test/basic_tests.py
@@ -3,6 +3,7 @@ from lib.KNearestNeighbors import *
 test = KNearestNeighbors()
 test.load_data_from_csv("./data/iris_data.txt")
 
+# Proper loading test
 for i in range(len(test.list_of_points)):
     point = test.list_of_points[i]
-    print("Punkty: %s | klasa: %s" % (point.get_point_values(), point.get_point_class()))
+    print("Points: %s | classes: %s" % (point.get_point_values(), point.get_point_class()))

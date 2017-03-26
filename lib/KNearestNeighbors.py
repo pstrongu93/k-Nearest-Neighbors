4333b9b77c263ddc6de7e230a892b5b1e5294e33
r3 <wojciechregula@yahoo.pl>
2017-03-26 01:12:30 +0100

    Added test and training data

--- lib/KNearestNeighbors.py
+++ lib/KNearestNeighbors.py
@@ -1,12 +1,17 @@
 import csv
+import math
 
 
 class KNearestNeighbors:
 
-    def __init__(self):
-        self.list_of_points = []
+    def __init__(self, path_to_training_data="data/training.txt", path_to_test_data="data/test.txt", k_value=3):
+        self.list_of_training_points = []
+        self.list_of_test_points = []
+        self.k = k_value
+        self.load_data_from_csv(path_to_training_data, training=True)
+        self.load_data_from_csv(path_to_test_data, training=False)
 
-    def load_data_from_csv(self, path):
+    def load_data_from_csv(self, path, training):
         with open(path, mode="rt", encoding="utf-8") as file:  # Open file with text mode
             rows = csv.reader(file)
             for row in rows:
@@ -16,12 +21,21 @@ class KNearestNeighbors:
                 for number_of_value in range(len(row)-1):  # extract values
                     point_values.append(row[number_of_value])
                 point_class = row[len(row)-1]
-                self.list_of_points.append(Point(point_values, point_class))
+                if training is True:
+                    # Create the point instance and append the list of points
+                    self.list_of_training_points.append(Point(point_values, point_class))
+                else:
+                    self.list_of_test_points.append(Point(point_values, point_class))
 
-    def calculate_euclidean_distance(self, point_1, point_2):
-        pass
+    @staticmethod
+    def calculate_euclidean_distance(point_1, point_2):
+        distance = 0.0
+        dimensions = len(point_1.list_of_values)
+        for i in range(dimensions):
+            distance += pow((point_1.get_point_value(i) - point_2.get_point_value(i)), 2)
+        return math.sqrt(distance)
 
-    def get_k_nearest_neighbors(self, k):
+    def get_k_nearest_neighbors(self):
         pass
 
     def get_response(self):
@@ -42,7 +56,7 @@ class Point:
         elif number_of_value < 0:
             raise Exception("Too small number of value")
         else:
-            return self.list_of_values[number_of_value]
+            return float(self.list_of_values[number_of_value])
 
     def get_point_values(self):
         return self.list_of_values
--- test/basic_tests.py
+++ test/basic_tests.py
@@ -1,9 +1,15 @@
 from lib.KNearestNeighbors import *
 
 test = KNearestNeighbors()
-test.load_data_from_csv("./data/iris_data.txt")
 
 # Proper loading test
-for i in range(len(test.list_of_points)):
-    point = test.list_of_points[i]
+for i in range(len(test.list_of_training_points)):
+    point = test.list_of_training_points[i]
     print("Points: %s | classes: %s" % (point.get_point_values(), point.get_point_class()))
+
+point_1 = test.list_of_training_points[1]
+point_2 = test.list_of_training_points[2]
+
+distance = test.calculate_euclidean_distance(point_1, point_2)
+print(distance)
+
